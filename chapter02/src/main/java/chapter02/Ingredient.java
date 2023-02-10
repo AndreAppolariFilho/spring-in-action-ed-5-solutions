a6a3fb48d0735279662560924e43c8bd25f2bf12
AndreAppolariFilho <andre.appolarif@gmail.com>
2023-02-10 16:43:24 -0300

    Added: content from chapter02

---
+++ chapter02/src/main/java/chapter02/Ingredient.java
@@ -0,0 +1,16 @@
+package chapter02;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+    public static enum Type{
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+
+    private final String id;
+    private final String name;
+    private final Type type;
+}
